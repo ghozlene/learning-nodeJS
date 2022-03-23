484622d82685c7667489c004aa3cd8565dc21a86
Achref <medachrefg@gmail.com>
2022-03-23 15:51:04 +0100

    blocking&No blocking code

--- app.js
+++ app.js
@@ -19,20 +19,20 @@ const server = http.createServer((req, res) => {
 			body.push(chunk);
 		});
 
-		req.on('end', () => {
+		return req.on('end', () => {
 			const parseBody = Buffer.concat(body).toString();
 			const message = parseBody.split('=')[1];
-			fs.writeFileSync('message.txt', message);
+			fs.writeFile('message.txt', message, () => {
+				res.statusCode = 302;
+				res.setHeader('location', '/');
+				return res.end();
+			});
 		});
-
-		res.statusCode = 302;
-		res.setHeader('location', '/');
-		return res.end();
 	}
 	res.setHeader('Content-Type', 'text/html');
 	res.write('<html>');
 	res.write(
-		'<head><title>my page</title></head><body><h1>my first course</h1></body>'
+		'<head><title>your message is already on your file </title></head><body><h1>my first course</h1></body>'
 	);
 
 	res.write('</html>');
