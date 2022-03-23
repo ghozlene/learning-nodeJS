6df15b9723823db6fd6f24162dbb279a0955771f
Achref <medachrefg@gmail.com>
2022-03-23 12:44:56 +0100

    sending response to the client

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-	console.log(req);
+	// console.log(req);
+	res.setHeader('Content-Type', 'text/html');
+	res.write('<html>');
+	res.write(
+		'<head><title>my page</title></head><body><h1>my first course</h1></body>'
+	);
+
+	res.write('</html>');
+	res.end();
 });
 
 server.listen(3000);
