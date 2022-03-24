b296c083ab4b0dcac95af69592639b1b9ee4882d
Achref <medachrefg@gmail.com>
2022-03-24 10:30:22 +0100

    parsing uncoming requests

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/', (req, res, next) => {
-	console.log('this always RUN');
+	// console.log('this always RUN');
 	next();
 });
+app.use('/product', (req, res, next) => {
+	console.log(req.body);
+	res.redirect('/');
+});
 
-app.use('/product-overview', (req, res, next) => {
+app.use('/add-product', (req, res, next) => {
 	console.log('this is first middleware');
-	res.send('<h1>product Overview</h1>');
+	res.send(
+		'<form action="/product" method="POST"><input type="text" name="title"><button type="submit">send</button></form>'
+	);
 });
 
 app.use('/', (req, res, next) => {
