49bd4d20f9e7820867893b50a75ed0a526022de8
Achref <medachrefg@gmail.com>
2022-03-24 12:34:27 +0100

    serving html pages(using sendFile and path.join)

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,14 @@
 const express = require('express');
-
+const path = require('path');
 const router = express.Router();
 
+router.get('/add-product', (req, res, next) => {
+	console.log('this is first middleware');
+	res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+});
 router.post('/add-product', (req, res, next) => {
 	console.log(req.body);
 	res.redirect('/');
 });
-router.get('/add-product', (req, res, next) => {
-	console.log('this is first middleware');
-	res.send(
-		'<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">send</button></form>'
-	);
-});
+
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
 	console.log('this is the second middleware');
 
-	res.send('<h1>hello nodejs</h1>');
+	res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
