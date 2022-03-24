19a68dc1b7d4d400741806d3111acc013cde2233
Achref <medachrefg@gmail.com>
2022-03-24 14:23:01 +0100

    adding new styles to our pages(css-html) and sharing Data Across requests (js files)

--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-	console.log('this is the second middleware');
+	console.log('shop.js: ', adminData.products);
+	// console.log('this is the second middleware');
 
 	res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
