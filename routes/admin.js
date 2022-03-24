805971c2712b0cd7b717294e0073b5d1314f1c47
Achref <medachrefg@gmail.com>
2022-03-24 11:41:10 +0100

    filtring paths

--- routes/admin.js
+++ routes/admin.js
@@ -2,16 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
 	console.log(req.body);
 	res.redirect('/');
 });
-
 router.get('/add-product', (req, res, next) => {
 	console.log('this is first middleware');
 	res.send(
-		'<form action="/product" method="POST"><input type="text" name="title"><button type="submit">send</button></form>'
+		'<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">send</button></form>'
 	);
 });
-
 module.exports = router;
