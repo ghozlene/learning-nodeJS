2c6f148d392ec5f711588e783f66f18353e1e3ff
Achref <medachrefg@gmail.com>
2022-03-24 12:51:45 +0100

    using helper function for navigation

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+
+const rootDir = require('../util/path');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
 	console.log('this is the second middleware');
 
-	res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+	res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
