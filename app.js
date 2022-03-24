0f7682e77f8f671adfc21f689fa885e6b910c46a
Achref <medachrefg@gmail.com>
2022-03-24 11:06:19 +0100

    adding 404 not found page

--- app.js
+++ app.js
@@ -10,4 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res) => {
+	res.status(404).send(`<h1>  not found page</h1>`);
+});
 app.listen(3000);
