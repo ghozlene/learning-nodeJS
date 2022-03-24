66cac698546ca2da2e718f8768ed381bd0830519
Achref <medachrefg@gmail.com>
2022-03-24 12:39:36 +0100

    returning 404 pages

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+const path = require('path');
+
 const express = require('express');
+
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -11,6 +14,6 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res) => {
-	res.status(404).send(`<h1>  not found page</h1>`);
+	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 app.listen(3000);
