832b3ced7f9051cc645c6664b69c9fc92632bd9f
Achref <medachrefg@gmail.com>
2022-03-25 20:42:57 +0100

    Registering the Routes

--- routes/admin.js
+++ routes/admin.js
@@ -3,11 +3,13 @@ const path = require('path');
 
 const rootDir = require('../util/path');
 
-const productController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 const router = express.Router();
 
-router.get('/add-product', productController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
 
-router.post('/add-product', productController.postAddProduct);
+router.get('/products', adminController.getProducts);
+
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
