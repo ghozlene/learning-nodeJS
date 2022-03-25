e80e596fc07d90342119d7b2c1bc6fad30a3d271
Achref <medachrefg@gmail.com>
2022-03-25 12:53:19 +0100

    working with handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -7,6 +7,7 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+	const products = adminData.products;
 	console.log('shop.js: ', adminData.products);
 	// console.log('this is the second middleware');
 
@@ -15,6 +16,9 @@ router.get('/', (req, res, next) => {
 		prods: adminData.products,
 		pageTitle: 'Shop',
 		path: '/',
+		hasProducts: products.length > 0,
+		activeShop: true,
+		productCSS: true,
 	});
 });
 
