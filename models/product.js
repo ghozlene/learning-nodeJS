cfae4b8a8f2c8c59e689c0dd0530fedfd046855e
Achref <medachrefg@gmail.com>
2022-03-25 15:42:49 +0100

    fatching data from file via the model

--- models/product.js
+++ models/product.js
@@ -1,13 +1,36 @@
+const path = require('path');
+const fs = require('fs');
+
 const products = [];
+const p = path.join(
+	path.dirname(require.main.filename),
+	'data',
+	'products.json'
+);
 module.exports = class Product {
 	constructor(title) {
 		this.title = title;
 	}
 
 	save() {
-		products.push(this);
+		fs.readFile(p, (err, fileContent) => {
+			let products = [];
+			if (!err) {
+				products = JSON.parse(fileContent);
+			}
+			products.push(this);
+			fs.writeFile(p, JSON.stringify(products), (err) => {
+				console.log(err);
+			});
+		});
 	}
-	static fetchAll() {
-		return products;
+
+	static async fetchAll(cb) {
+		await fs.readFile(p, (err, fileContent) => {
+			if (err) {
+				cb([]);
+			}
+			cb(JSON.parse(fileContent));
+		});
 	}
 };
