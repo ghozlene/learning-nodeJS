3b954fb7c11e0067e2138dc6fccea973cec5c6e9
Achref <medachrefg@gmail.com>
2022-03-25 16:23:06 +0100

    refactoring the file storage code

--- models/product.js
+++ models/product.js
@@ -1,36 +1,37 @@
 const path = require('path');
 const fs = require('fs');
 
-const products = [];
 const p = path.join(
 	path.dirname(require.main.filename),
 	'data',
 	'products.json'
 );
+
+const getProductFromFile = (cb) => {
+	fs.readFile(p, (err, fileContent) => {
+		if (err) {
+			cb([]);
+		} else {
+			cb(JSON.parse(fileContent));
+		}
+	});
+};
 module.exports = class Product {
 	constructor(title) {
 		this.title = title;
 	}
 
 	save() {
-		fs.readFile(p, (err, fileContent) => {
-			let products = [];
-			if (!err) {
-				products = JSON.parse(fileContent);
-			}
+		getProductFromFile((products) => {
 			products.push(this);
+
 			fs.writeFile(p, JSON.stringify(products), (err) => {
 				console.log(err);
 			});
 		});
 	}
 
-	static async fetchAll(cb) {
-		await fs.readFile(p, (err, fileContent) => {
-			if (err) {
-				cb([]);
-			}
-			cb(JSON.parse(fileContent));
-		});
+	static fetchAll(cb) {
+		getProductFromFile(cb);
 	}
 };
