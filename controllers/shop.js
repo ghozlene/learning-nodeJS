240ebb0eb7ceacd2364580faf1fcd5555c908c3c
Achref <medachrefg@gmail.com>
2022-03-26 15:06:46 +0100

    Rendering product detail view

--- controllers/shop.js
+++ controllers/shop.js
@@ -14,9 +14,12 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
 	const prodId = req.params.productId;
 	Product.findById(prodId, (product) => {
-		console.log(product);
+		res.render('shop/product-detail', {
+			path: '/products',
+			product: product,
+			pageTitle: product.id,
+		});
 	});
-	res.redirect('/');
 };
 
 exports.getIndex = (req, res, next) => {
