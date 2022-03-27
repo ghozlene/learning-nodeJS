579b3bb12b4478f0a279629fa3de09e25f894fb5
Achref <medachrefg@gmail.com>
2022-03-27 15:09:42 +0100

    Adding the Product Delete functionality

--- models/product.js
+++ models/product.js
@@ -46,6 +46,15 @@ module.exports = class Product {
 		});
 	}
 
+	static deleteById(id, cb) {
+		getProductFromFile((products) => {
+			const updatedProducts = products.filter((p) => p.id !== id);
+			fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+				if (!err) {
+				}
+			});
+		});
+	}
 	static fetchAll(cb) {
 		getProductFromFile(cb);
 	}
