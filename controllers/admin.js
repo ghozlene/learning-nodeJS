98bc022277089a58dc7d7531fc720f7225b7986f
Achref <medachrefg@gmail.com>
2022-03-29 16:15:49 +0100

    Creating a product in our DB

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,7 +14,9 @@ exports.postAddProduct = (req, res, next) => {
 	const price = req.body.price;
 	const description = req.body.description;
 	const product = new Product(title, imageUrl, description, price);
-	product.save();
+	product.save().then((res) => {
+		console.log('product created');
+	});
 	res.redirect('/');
 };
 
