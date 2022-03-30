83453c82e1c02b1af17f1947207830c1d0e1b59b
Achref <medachrefg@gmail.com>
2022-03-30 21:59:07 +0100

    deleting Cart Items

--- controllers/shop.js
+++ controllers/shop.js
@@ -81,10 +81,12 @@ exports.postCart = (req, res, next) => {
 };
 exports.postDeleteProduct = (req, res, next) => {
 	const prodId = req.body.productId;
-	Product.findById(prodId, (product) => {
-		Cart.deleteProduct(prodId, product.price);
-		res.redirect('/cart');
-	});
+	req.user
+		.deleteItemFromCart(prodId)
+		.then((result) => {
+			res.redirect('/cart');
+		})
+		.catch((err) => console.log(err));
 };
 exports.getOrders = (req, res, next) => {
 	res.render('shop/orders', {
--- routes/shop.js
+++ routes/shop.js
@@ -16,7 +16,7 @@ router.get('/cart', shopController.getCart);
 
 router.post('/cart', shopController.postCart);
 
-// router.post('/cart-delete-item', shopController.postDeleteProduct);
+router.post('/cart-delete-item', shopController.postDeleteProduct);
 
 // router.get('/orders', shopController.getOrders);
 
