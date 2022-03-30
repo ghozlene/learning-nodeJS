1bbedabe4417df7fdcd618d5a635b2d0eae5599c
Achref <medachrefg@gmail.com>
2022-03-30 22:38:57 +0100

    adding relational order data

--- models/user.js
+++ models/user.js
@@ -95,9 +95,17 @@ class User {
 	}
 	addOrder() {
 		const db = getDb();
-		return db
-			.collection('orders')
-			.insertOne(this.cart)
+		return this.getCart()
+			.then((products) => {
+				const order = {
+					items: products,
+					user: {
+						_id: new ObjectId(this._id),
+						username: this.username,
+					},
+				};
+				return db.collection('orders').insertOne(order);
+			})
 			.then((result) => {
 				this.cart = { items: [] };
 				return db
