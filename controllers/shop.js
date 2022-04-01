b3166fe955ca759df58941786bf0130a880cc9f1
Achref <medachrefg@gmail.com>
2022-04-01 14:46:58 +0100

    Creating Getting Order

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const Cart = require('../models/cart');
 const Product = require('../models/product');
+const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
 	Product.find()
@@ -109,7 +110,22 @@ exports.getOrders = (req, res, next) => {
 };
 exports.postOrder = (req, res, next) => {
 	req.user
-		.addOrder()
+		.populate('cart.items.productId')
+
+		.then((user) => {
+			const products = user.cart.items.map((i) => {
+				return { quantity: i.quantity, product: i.productId };
+			});
+			const order = new Order({
+				user: {
+					name: req.user.name,
+					userId: req.user,
+				},
+				products: products,
+			});
+			return order.save();
+		})
+
 		.then(
 			res.render('shop/orders', {
 				path: '/orders',
