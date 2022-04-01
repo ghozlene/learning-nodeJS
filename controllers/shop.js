2c934fa9a3d88997b9438fffea04d8d704f11a9b
Achref <medachrefg@gmail.com>
2022-04-01 14:50:16 +0100

    Storing all Order related Data

--- controllers/shop.js
+++ controllers/shop.js
@@ -114,7 +114,7 @@ exports.postOrder = (req, res, next) => {
 
 		.then((user) => {
 			const products = user.cart.items.map((i) => {
-				return { quantity: i.quantity, product: i.productId };
+				return { quantity: i.quantity, product: { ...i.productId._doc } };
 			});
 			const order = new Order({
 				user: {
